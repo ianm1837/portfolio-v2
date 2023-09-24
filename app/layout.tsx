29c17b575a4ddb23096f1f08c2bbf6e3d3dd3520
ianm1837 <ian@iwm.dev>
2023-09-23 18:40:51 -0700

    update metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,37 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Ian Meyer",
+  metadataBase: new URL("https://ianwmeyer.com"),
   description: "Full Stack Web Developer",
+  themeColor: "#0F1729",
+  openGraph: {
+    title: "Ian Meyer",
+    description: "Full Stack Web Developer",
+    url: "https://ianwmeyer.com",
+    siteName: "ianwmeyer.com",
+    images: [
+      {
+        url: "https://ianwmeyer.com/meta-img.png",
+        width: 477,
+        height: 440,
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  robots: {
+    index: true,
+    follow: false,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: false,
+      noimageindex: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
 };
 
 export default function RootLayout({
